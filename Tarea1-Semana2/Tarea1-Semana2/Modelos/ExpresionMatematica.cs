9e252420784303c0c2a60b265fef7ebf524b93c0
AleJcA <alexander18072003m@gmail.com>
2023-01-25 21:14:09 -0600

    Commit Summary

---
+++ Tarea1-Semana2/Tarea1-Semana2/Modelos/ExpresionMatematica.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tarea1_Semana2.Modelos
+{
+    public class ExpresionMatematica
+    {
+       public double numero1 { get; set; }
+       public double numero2 { get; set; }
+
+        public double suma, resta, div, multi, max, min;
+
+        public double Suma()
+        {
+            suma = numero1 + numero2;
+
+            return suma;
+        }
+
+        public double Resta()
+        {
+            resta = numero1 - numero2;
+
+            return resta;
+        }
+
+        public double Divison()
+        {
+            div = numero1 / numero2;
+
+            return div;
+        }
+
+        public double Multiplicacion()
+        {
+            multi = numero1 * numero2;
+
+            return multi;
+        }
+
+        public void Maximo()
+        {
+            if (numero1 > numero2)
+            {
+                Console.WriteLine(max = numero1);
+            }if(numero2 > numero1)
+            {
+                Console.WriteLine(max = numero2);
+            }
+        }
+
+        public void Minimo()
+        {
+            if (numero1 < numero2)
+            {
+                Console.WriteLine(min = numero1);
+            }if (numero2 < numero1)
+            {
+                Console.WriteLine(min = numero2);
+            }
+        }
+        public string toString()
+        {
+            return $"Suma:  {suma}" +
+                   $" Resta: {resta}" +
+                   $" Division: {div}" +
+                   $" Multiplicacion: {multi}" +
+                   $" Maximo: {max}" +
+                   $" Minimo: {min}";
+
+
+        }
+    }
+}
---
+++ Tarea1-Semana2/Tarea1-Semana2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Tarea1_Semana2.Modelos;
+
+List<ExpresionMatematica> Expresiones = new List<ExpresionMatematica>();
+
+
+ExpresionMatematica EXP1 = new ExpresionMatematica()
+{
+    numero1 = 2,
+    numero2 = 3
+};
+ExpresionMatematica EXP2 = new ExpresionMatematica()
+{
+    numero1 = 10,
+    numero2 = 5
+};
+Expresiones.Add(EXP1);
+Expresiones.Add(EXP2);
+
+EXP1.Suma();
+EXP1.Resta();
+EXP1.Divison();
+EXP1.Multiplicacion();
+EXP1.Maximo();
+EXP1.Minimo();
+
+EXP2.Suma();
+EXP2.Resta();
+EXP2.Divison();
+EXP2.Multiplicacion();
+EXP2.Maximo();
+EXP2.Minimo();
+
+foreach (ExpresionMatematica x in Expresiones)
+{
+    
+    Console.WriteLine(x.toString());
+}
+
+
